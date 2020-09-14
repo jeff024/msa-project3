1da63dbf3d1a1394349959631477f4a080ec0199
haodong lu <lhd32262175@gmail.com>
2020-09-14 19:17:28 +1000

    database built

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 import os
-# import urllib.parse
+import urllib.parse
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# db_string = os.environ['dbstring']
-# params = urllib.parse.quote_plus(db_string)
+db_string = os.environ['dbstring']
+params = urllib.parse.quote_plus(db_string)
 
-# # initialization
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'supersecret'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+# app.config['SQLALCHEMY_DATABASE_URI'] =\
+#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
